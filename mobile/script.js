d3d77d300f946110307956ba820988bfa820258c
Emanuel Bravo <emanuelbravo0706@gmail.com>
2025-08-20 12:23:29 -0300

    style: carrinho no header e toast add no carrinho
    Alteração do Header para adicionar o link dinamicamente do carrinho caso tenha algum item
    Adiciona Toast que aparece ao adicionar item ao carrinho
    
    Faz a alteração em todas as páginas

--- mobile/script.js
+++ mobile/script.js
@@ -14,6 +14,16 @@ function toggleArticle(articleID) {
     }
 }
 
+
+function updateCartIcon() {
+    let cart = JSON.parse(localStorage.getItem('cart')) || [];
+    if (cart.length > 0) {
+        document.getElementById('cart-link').innerHTML = `
+            <img src="imgs/icones/carrinho.png" alt="Carrinho" class="menu-icon icon-2">
+        `;
+    }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     const buttons = document.querySelectorAll('.add-carrinho');
 
@@ -43,9 +53,30 @@ document.addEventListener('DOMContentLoaded', () => {
                 }
 
                 localStorage.setItem('cart', JSON.stringify(cart));
-                alert(`${item.nome} adicionado ao carrinho!`);
-                window.location.href = 'carrinho.html';
+                
+                // window.location.href = 'carrinho.html';
+
+                const cartToastContainer = document.getElementById('cart-toast-container')
+                cartToastContainer.innerHTML = `
+                    <div id="liveToast" class="toast align-items-center rounded-3" role="alert" aria-live="assertive" aria-atomic="true">
+                        <div class="toast-header bg-success-subtle">
+                            <strong class="me-auto">Item adicionado ao carrinho</strong>
+                            <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+                        </div>
+                        <div class="toast-body bg-light rounded-3">
+                            ${itemName}
+                        </div>
+                    </div>
+                    `;
+
+                const toastLiveExample = document.getElementById('liveToast')
+                const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+                toastBootstrap.show()
+                updateCartIcon()
             });
         });
     }
+
+    
+    updateCartIcon();
 });
