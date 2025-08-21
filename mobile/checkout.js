52eea2795693966e482f53c5d25dd60384bd3832
Emanuel Bravo <emanuelbravo0706@gmail.com>
2025-08-21 10:33:22 -0300

    style: checkout e pagamento

--- mobile/checkout.js
+++ mobile/checkout.js
@@ -4,15 +4,15 @@ function pagamento () {
     const totalFrete = 10 + totalAtual;
     blocoValor.innerHTML = '';
     blocoValor.innerHTML += `
-        <div class="d-flex flex-row justify-content-between pt-2 pb-2 ps-2 pe-2">
+        <div class="d-flex flex-row justify-content-between pt-2">
             <p>Subtotal</p>
             <p>R$ ${totalAtual.toFixed(2)}</p>
         </div>
-        <div class="d-flex flex-row justify-content-between pt-2 pb-2 ps-2 pe-2">
+        <div class="d-flex flex-row justify-content-between p-2">
             <p>Frete</p>
             <p>R$ 10.00</p>
         </div>
-        <div class="d-flex flex-row justify-content-between pt-2 pb-2 ps-2 pe-2">
+        <div class="d-flex flex-row justify-content-between p-2">
             <p>Total</p>
             <p>R$ ${(totalFrete.toFixed(2))}</p>
         </div>
